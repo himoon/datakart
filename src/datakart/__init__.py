170504354987d6b71707c783b02174a333d319ec
hyun1moon <hyunil.moon@gmail.com>
2023-12-14 01:36:46 +0900

    Add jusogokr

--- src/datakart/__init__.py
+++ src/datakart/__init__.py
@@ -1,13 +1,15 @@
 from .core.datagokr import Datagokr
 from .core.ecos import Ecos
+from .core.jusogokr import Jusogokr
 from .core.naver import Naver
 from .core.sgis import Sgis
 
 __all__ = [
     "Datagokr",
     "Ecos",
+    "Jusogokr",
     "Naver",
     "Sgis",
 ]
 
-__version__ = "1.4.1"
+__version__ = "1.5.1"
