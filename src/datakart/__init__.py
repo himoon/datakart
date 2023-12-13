b09a24beb9f975af8a1f4955e9913e9cbf185a71
hyun1moon <hyunil.moon@gmail.com>
2023-12-13 23:40:05 +0900

    Update to 1.4.1

--- src/datakart/__init__.py
+++ src/datakart/__init__.py
@@ -1,7 +1,13 @@
 from .core.datagokr import Datagokr
 from .core.ecos import Ecos
+from .core.naver import Naver
 from .core.sgis import Sgis
 
-__all__ = ["Datagokr", "Ecos", "Sgis"]
+__all__ = [
+    "Datagokr",
+    "Ecos",
+    "Naver",
+    "Sgis",
+]
 
-__version__ = "1.3.1"
+__version__ = "1.4.1"
